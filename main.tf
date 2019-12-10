234b549b9e4505672140eceb8d3b39ae74502b04
Steven Nemetz <steven@appzen.com>
2019-12-10 10:31:56 -0800

    Use only top_level_domain if account doesn not have a -

--- main.tf
+++ main.tf
@@ -1,3 +1,6 @@
 locals {
-  parent_zone = "${replace(var.account_name, "/.*-/", "")}.${var.top_level_domain}"
+  parent_zone = "${length(split("-", var.account_name)) > 1
+    ? "${replace(var.account_name, "/.*-/", "")}.${var.top_level_domain}"
+    : "${var.top_level_domain}"
+  }"
 }
